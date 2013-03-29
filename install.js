2d20d94a4e2221db510f905f63d1a0cc7cf078e8
Christoph Dorn <christoph@christophdorn.com>
2013-03-29 12:37:03 -0700

    fix instal

--- install.js
+++ install.js
@@ -1,16 +1,10 @@
 
-const SM = require("sm");
-const PM = SM.for(__dirname, {
-	// TODO: Check for `process.env.SM_VERBOSE`.
-	verbose: true,
-	// TODO: Check for `process.env.SM_NOW`.
-	now: true
-});
+const PINF = require("sm/node_modules/pinf").for(module);
 
 
 exports.main = function(callback) {
 	// Install platform specific dependencies.
-	PM.resolve("dev-" + process.platform + "/package.json", function(err, path) {
+	PINF.resolve("dev-" + process.platform + "/package.json", function(err, path) {
 		if (err) {
 			if (/Package not found and not declared/.test(err.message)) {
 				return callback(new Error("Platform '" + process.platform + "' not yet supported!"));
